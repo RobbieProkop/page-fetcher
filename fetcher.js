464808cce9e28fed8f40edc27d5227ab31cda272
Robbie Prokop <robbieprokop@gmail.com>
2022-04-14 16:57:38 +0000

    updated to use callbacks and not be hardcoded

--- fetcher.js
+++ fetcher.js
@@ -1,13 +1,34 @@
 const request = require("request")
 const fs = require('fs')
-const content = "Jeez, I sure am tired today."
+// const content = "Jeez, I sure am tired today."
+const url = process.argv[2];
+const path = process.argv[3];
 
-request('http://www.nepallife.org', (error, response, body) =>{
-  fs.writeFile("./test.txt", body, error => {
-    if(error){
-      console.log(error);
-      return
-    }
+const fetchWebsite = function (url, path){
+  request(url, (error, response, body) =>{
+    if(error) return console.log(`You got an error: ${error}`)
+    if(response >200 && response < 500) return console.log(`Here's a response for you: ${response}`)
+    fs.writeFile(path, body, error => {
+      const byteSize = body.length;
+      if(error){
+        console.log(`You got an error: ${error}`);
+        return
+      }
+      console.log(`You have successfully downloaded ${url} (${byteSize} bytes) to ${path}`)
+  
+    })
+  
   })
 
-})
+}
+fetchWebsite(url, path)
+
+// request('http://www.nepallife.org', (error, response, body) =>{
+//   fs.writeFile("./test.txt", body, error => {
+//     if(error){
+//       console.log(error);
+//       return
+//     }
+//   })
+
+// })
